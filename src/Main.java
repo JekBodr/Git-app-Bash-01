3baf45cafd419a97a20ebcb1cbdad4287f09f591
JekBodr <jekbodr@gmail.com>
2022-02-22 22:37:29 +0200

    Add substraction and division

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("My git app. Super!");
-        int a = 5;
-        int b = 7;
+        int a = 10;
+        int b = 2;
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
+        int e = subtraction(a, b);
+        System.out.println(e);
+        int f = division(a, b);
+        System.out.println(f);
     }
     private static int add(int a, int b) {
         return a + b;
     }
     private static int multiply(int a, int b) {
         return a * b;
+    }
+    private static int subtraction(int a, int b) {
+        return a - b;
+    }
+    private static int division(int a, int b) {
+        return a / b;
     }
 }
